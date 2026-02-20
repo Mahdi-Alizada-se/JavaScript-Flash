df60930295250f85c68ad2424897186c9d1ac29f
Mahdi Alizada <mahdi.alizada.se@gmail.com>
2026-02-20 12:17:52 +0430

    Implement navigation menu toggle functionality

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector("#open-nav-menu").addEventListener("click", function () {
+  //   alert("helloworld");
+  document.querySelector("header nav .wrapper ").classList.toggle("nav-open");
+});
+document
+  .querySelector("#close-nav-menu")
+  .addEventListener("click", function () {
+    //   alert("helloworld");
+    document.querySelector("header nav .wrapper ").classList.toggle("nav-open");
+  });
